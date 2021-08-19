7d01e03694004dce0eb66cd5e1ce8b90eacd30da
Aruka7 <58134354+Aruka7@users.noreply.github.com>
2021-08-19 17:54:43 +0700

    Доделал строки и команды для свича актуатора

--- ArduinoGarden/lib/Manager/ActuatorSwitch.cpp
+++ ArduinoGarden/lib/Manager/ActuatorSwitch.cpp
@@ -1,4 +1,4 @@
-#include "ActuatorSwitch.h".h"
+#include "ActuatorSwitch.h"
 
 
 ActuatorSwitch::ActuatorSwitch(bool switchingType, Model* model) : AbstractOperation(model){
@@ -12,4 +12,45 @@ void ActuatorSwitch::Execute(){
 
     model->_relayStatusStorage->SetActuator(_switchingType ? 1 : 0);
     IsCompleted = true;
+}
+char* ActuatorSwitch::GetString(){
+    char* tmp = new char[10]{"ACT "};
+    char switcher[5];
+    if(_switchingType){
+        strcat(switcher, "Open");
+    }
+    else{
+        strcat(switcher, "Close");
+    }
+    strcat(tmp, switcher);
+    return tmp;
+}
+uint8_t ActuatorSwitch::GetCursorPosition(){
+    return _cursorPosition;
+}
+void ActuatorSwitch::HandleCommand(EncoderAction action){
+    if(action == EncoderAction::Left){
+        if(_cursorPosition == 0) _cursorPosition = ACTSWITCH_CURSOR_POS_LENGTH;
+        _cursorPosition--;
+        return;
+    }
+    if(action == EncoderAction::Right){
+        if(_cursorPosition == ACTSWITCH_CURSOR_POS_LENGTH - 1) {
+            _cursorPosition = 0;
+            return;
+        }
+        _cursorPosition++;
+        return;
+    }
+    if((action == EncoderAction::LeftHold) || (action == EncoderAction::RightHold)){
+        switch (_cursorPosition)
+        {
+        case 0: return;
+        case 1:
+            _switchingType = !_switchingType;
+            return;
+        default:
+            break;
+        }
+    }
 }
--- ArduinoGarden/lib/Manager/ActuatorSwitch.h
+++ ArduinoGarden/lib/Manager/ActuatorSwitch.h
@@ -4,14 +4,21 @@
 #include "AbstractOperation.h"
 #include <stdint.h>
 #include "Model.h"
+#define ACTSWITCH_CURSOR_POS_LENGTH 2
 class ActuatorSwitch : public AbstractOperation
 {
 private:
+    uint8_t _cursorPosition;
+    uint8_t _cursorPositions[ACTSWITCH_CURSOR_POS_LENGTH] 
+    = {0, 4};
     bool _switchingType; //Type of direction switching 1 is mean that act will turn outside and otherwise 0 mean inside
 public:
     ActuatorSwitch(bool switchingType, Model* model);
     ~ActuatorSwitch() override;
     void Execute() override;
+    char* GetString() override;
+    uint8_t GetCursorPosition() override;
+    void HandleCommand(EncoderAction action) override;
 };
 
 #endif
